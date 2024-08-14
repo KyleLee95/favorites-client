816bbf0f6a3d5faf43e8babdc8fdb5be9f164458
Kyle <kyle@kylelee.dev>
2024-08-14 16:54:15 -0400

    add amplify. add portrait screen lock to androidmanifest

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "favoritesclient5b0d3364": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
